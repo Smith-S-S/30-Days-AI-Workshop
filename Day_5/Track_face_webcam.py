199fc1055dd7d63c4927e05a50ec9f6a522e3fbc
Smith-S-S <80092760+Smith-S-S@users.noreply.github.com>
2022-11-06 11:59:03 +0530

    Update Track_face_webcam.py

--- Day_5/Track_face_webcam.py
+++ Day_5/Track_face_webcam.py
@@ -1,1 +1,21 @@
+import cv2
+train = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
+vd = cv2.VideoCapture(0)
+
+while True:
+    success, frame = vd.read()
+    if success== True:
+        gr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        com = train.detectMultiScale(gr)
+        #print (com)
+        #print (frame)
+        for x, y, w, h in com:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
+        cv2.imshow("s", frame)
+        key = cv2.waitKey(1)
+        if key == 32:
+            break
+    else:
+        print ("completed:")
+        break
