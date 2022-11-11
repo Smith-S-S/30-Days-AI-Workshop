29cb6464e3d281dd914967fb9e05decd818547b3
Smith-S-S <80092760+Smith-S-S@users.noreply.github.com>
2022-11-11 22:28:32 +0530

    Update Mobile_Net_SSD.py

--- Day-11 Neural Network/Pre Trained Model/Mobile_Net_SSD.py
+++ Day-11 Neural Network/Pre Trained Model/Mobile_Net_SSD.py
@@ -1,8 +1,3 @@
-# USAGE
-# python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
-
-# import the necessary packages
-
 import numpy as np
 import imutils
 import time
@@ -14,51 +9,51 @@ model = "MobileNetSSD_deploy.caffemodel"
 confThresh=0.2
 
 # initialize the list of class labels MobileNet SSD was trained to
-# detect, then generate a set of bounding box colors for each class
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 	"sofa", "train", "tvmonitor"]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+# picking random colour for each class
 
 # load our serialized model from disk
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(prototxt,model)
 
-# initialize the video stream, allow the cammera sensor to warmup,
-# and initialize the FPS counter
+# initialize the video stream
 print("[INFO] starting video stream...")
 vs = cv2.VideoCapture(0)
-# vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 
 
 # loop over the frames from the video stream
 while True:
 
-    # grab the frame from the threaded video stream and resize it
+    # grab the frame
     # to have a maximum width of 400 pixels
 	_,frame = vs.read()
 	frame = imutils.resize(frame, width=500)
 
 	# grab the frame dimensions and convert it to a blob
-	(h, w) = frame.shape[:2]
+	(h, w) = frame.shape[:2] # to get the height and weight
 
 
 	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-		0.007843, (300, 300), 127.5)
+		0.007843, (300, 300), 127.5) #prebuit model frame is 300x300
+	#conveting to blop image
 
 	# pass the blob through the network and obtain the detections and
 	# predictions
-	net.setInput(blob)
-	detections = net.forward()
+	net.setInput(blob) # make as an input
+	detections = net.forward() # geat all the info about the image
     #detShape = detections.shape[2]
 
 	# loop over the detections
 	for i in np.arange(0, detections.shape[2]):
 		# extract the confidence (i.e., probability) associated with
 		# the prediction
-		confidence = detections[0, 0, i, 2]
+		#print(i)
+		confidence = detections[0, 0, i, 2] # this give the confidence
 
 		# filter out weak detections by ensuring the `confidence` is
 		# greater than the minimum confidence
@@ -66,16 +61,18 @@ while True:
             # extract the index of the class label from the
             # `detections`, then compute the (x, y)-coordinates of
             # the bounding box for the object
-			idx = int(detections[0,0,i,1])
+			idx = int(detections[0,0,i,1]) # it give the index number
+
 
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+			(startX, startY, endX, endY) = box.astype("int") # it give the bounding box info(3:7[3,4,5,6])
 
 			# draw the prediction on the frame
 			label = "{}: {:.2f}%".format(CLASSES[idx],
-				confidence * 100)
+				confidence * 100)# get the name from CLASS
 			cv2.rectangle(frame, (startX, startY), (endX, endY),
 				COLORS[idx], 2)
+			
 			y = startY - 15 if startY - 15 > 15 else startY + 15
 
 
