f9faaa77ea50c8ee6db0ff6a007c414980dd3e12
Smith-S-S <80092760+Smith-S-S@users.noreply.github.com>
2022-11-14 21:11:19 +0530

    Add files via upload

---
+++ Day-12 Neural Network/testing_the_model.py
@@ -0,0 +1,39 @@
+from keras.models import model_from_json
+import numpy as np
+from keras.preprocessing import image
+
+json_file= open('model.json','r')
+loaded_model_json= json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+model.load_weights("model.h5")
+print ("Loaded model from disk")
+
+def classify (img_file):
+    img_name=img_file
+    test_image= image.load_img(img_name,target_size = (64, 64))
+
+
+
+    test_image= image.img_to_array(test_image)
+    test_image=np.expand_dims(test_image, axis=0)
+    result= model.predict(test_image)
+    if result[0][0]== 1:
+        prediction="monkey"
+    else:
+        prediction="tiger"
+    print(prediction,img_file)
+
+
+import os
+path='images/test'
+files=[]
+#r=root,d=directories,f=files
+for r,d,f in os.walk (path) :
+    for file in f:
+        if'•jpeg' in file:
+            files.append(os.path.join(r,file))
+for f in files:
+    classify(f)
+    print("\n")
+
